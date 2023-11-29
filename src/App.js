9967a6068be1976f9ef64b269d84fd9d20674d39
Julien Audras <audras.julien@gmail.com>
2023-11-29 14:34:44 +0100

    proper first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/style.css";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import Logo from "./components/Logo";
+import { useSpring, animated } from "@react-spring/web";
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Logo /> */}
+
+      <Parallax pages={2} class="animation">
+        <ParallaxLayer offset={0.2} speed={0.5}>
+          <div className="animation_layer parallax" id="logoInt"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div
+            className="animation_layer parallax"
+            id="artback"
+            // style={{ zIndex: 1 }}
+          >
+            {/* <Logo /> */}
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.1} speed={0.5}>
+          <div className="animation_layer parallax" id="mountainsBg"></div>
+          <img src={logo} alt="logo" className="logo" />
+        </ParallaxLayer>
+        {/* <ParallaxLayer offset={0.2} speed={0.2}>
+          <div className="animation_layer parallax" id="logoInt"></div>
+        </ParallaxLayer> */}
+        <ParallaxLayer offset={0.3} speed={0.8}>
+          {/* <img src={logo} alt="logo" className="logo" /> */}
+          <div className="animation_layer parallax" id="mountainsFront"></div>
+          <span class="scroll-icon">
+            <span class="scroll-icon__dot"></span>
+          </span>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.38} speed={0.7}>
+          <div className="animation_layer parallax" id="mountainRight"></div>
+        </ParallaxLayer>
+        {/* <ParallaxLayer offset={0.08} speed={0.7}>
+          <div className="animation_layer parallax" id="birds"></div>
+        </ParallaxLayer> */}
+        <ParallaxLayer offset={0.51} speed={0.75}>
+          <div className="animation_layer parallax" id="treesFront"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.35} speed={0.86}>
+          <div className="animation_layer parallax" id="manClimbing"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.35} speed={0.8}>
+          <div className="animation_layer parallax" id="treesLine"></div>
+        </ParallaxLayer>
+        {/* <ParallaxLayer offset={0.96} speed={0.1}>
+          <div class="animation_layer parallax" id="blackbg"></div>
+        </ParallaxLayer> */}
+        {/* <ParallaxLayer offset={0} speed={0.35}>
+          <div class="animation_layer parallax" id="jungle5"></div>
+        </ParallaxLayer> */}
+        <ParallaxLayer offset={1} speed={0.25}>
+          {/* <TextBlock /> */}
+        </ParallaxLayer>
+      </Parallax>
+      {/* <section className="section intro">
+        <h2 className="section__title">About</h2>
+      </section> */}
     </div>
   );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,158 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <section className="introSection">
+        <p className="introSection__paragraph">
+            <span className ="introSection__paragraph--span">J'ai</span>
+            <span className ="introSection__paragraph--span">commencé</span>
+            <span className ="introSection__paragraph--span">mon</span>
+            <span className ="introSection__paragraph--span">chemin</span>
+            <span className ="introSection__paragraph--span">dans</span>
+            <span className ="introSection__paragraph--span">le</span>
+            <span className ="introSection__paragraph--span">monde</span>
+            <span className ="introSection__paragraph--span">du</span>
+            <span className ="introSection__paragraph--span">dévelpoppement</span>
+            <span className ="introSection__paragraph--span">web</span>
+            <span className ="introSection__paragraph--span">en</span>
+            <span className ="introSection__paragraph--span">autodidacte</span>
+            <span className ="introSection__paragraph--span">par</span>
+            <span className ="introSection__paragraph--span">pure</span>
+            <span className ="introSection__paragraph--span">curiosité.</span>
+            <span className ="introSection__paragraph--span">Rapidement,</span>
+            <span className ="introSection__paragraph--span">j'ai</span>
+            <span className ="introSection__paragraph--span">été</span>
+            <span className ="introSection__paragraph--span">fasciné</span>
+            <span className ="introSection__paragraph--span">par</span>
+            <span className ="introSection__paragraph--span">la</span>
+            <span className ="introSection__paragraph--span">puissance</span>
+            <span className ="introSection__paragraph--span">de</span>
+            <span className ="introSection__paragraph--span">la</span>
+            <span className ="introSection__paragraph--span">programmation</span>
+            <span className ="introSection__paragraph--span">et</span>
+            <span className ="introSection__paragraph--span">j'ai</span>
+            <span className ="introSection__paragraph--span">décidé</span>
+            <span className ="introSection__paragraph--span">de</span>
+            <span className ="introSection__paragraph--span">rejoindre</span>
+            <span className ="introSection__paragraph--span">la</span>
+            <span className ="introSection__paragraph--span">formation</span>
+            <span className ="introSection__paragraph--span">de</span>
+            <span className ="introSection__paragraph--span">développeur</span>
+            <span className ="introSection__paragraph--span">Front-End</span>
+            <span className ="introSection__paragraph--span">d'Open</span>
+            <span className ="introSection__paragraph--span">Classrooms.</span>
+        </p>
+        <p className="introSection__paragraph">
+            <span className ="introSection__paragraph--span">Parallèlement</span>
+            <span className ="introSection__paragraph--span">à</span>
+            <span className ="introSection__paragraph--span">cette</span>
+            <span className ="introSection__paragraph--span">formation,</span>
+            <span className ="introSection__paragraph--span">j'ai</span>
+            <span className ="introSection__paragraph--span">exploré</span>
+            <span className ="introSection__paragraph--span">d'autres</span>
+            <span className ="introSection__paragraph--span">cours</span>
+            <span className ="introSection__paragraph--span">et</span>
+            <span className ="introSection__paragraph--span">me</span>
+            <span className ="introSection__paragraph--span">suis</span>
+            <span className ="introSection__paragraph--span">lancé</span>
+            <span className ="introSection__paragraph--span">dans</span>
+            <span className ="introSection__paragraph--span">divers</span>
+            <span className ="introSection__paragraph--span">projets.</span>
+            <span className ="introSection__paragraph--span">Cette</span>
+            <span className ="introSection__paragraph--span">démarche</span>
+            <span className ="introSection__paragraph--span">m'a</span>
+            <span className ="introSection__paragraph--span">permis</span>
+            <span className ="introSection__paragraph--span">d'élargir</span>
+            <span className ="introSection__paragraph--span">mes</span>
+            <span className ="introSection__paragraph--span">compétences</span>
+            <span className ="introSection__paragraph--span">et</span>
+            <span className ="introSection__paragraph--span">d'appréhender</span>
+            <span className ="introSection__paragraph--span">le</span>
+            <span className ="introSection__paragraph--span">développement</span>
+            <span className ="introSection__paragraph--span">web</span>
+            <span className ="introSection__paragraph--span">sous</span>
+            <span className ="introSection__paragraph--span">des</span>
+            <span className ="introSection__paragraph--span">perspectives</span>
+            <span className ="introSection__paragraph--span">variées.</span>
+        </p>
+        <p className="introSection__paragraph">
+            <span className ="introSection__paragraph--span">Développeur</span>
+            <span className ="introSection__paragraph--span">enthousiaste,</span>
+            <span className ="introSection__paragraph--span">je</span>
+            <span className ="introSection__paragraph--span">fusionne</span>
+            <span className ="introSection__paragraph--span">ma</span>
+            <span className ="introSection__paragraph--span">passion</span>
+            <span className ="introSection__paragraph--span">pour</span>
+            <span className ="introSection__paragraph--span">le</span>
+            <span className ="introSection__paragraph--span">code</span>
+            <span className ="introSection__paragraph--span">avec</span>
+            <span className ="introSection__paragraph--span">une</span>
+            <span className ="introSection__paragraph--span">expérience</span>
+            <span className ="introSection__paragraph--span">entrepreneuriale.</span>
+        </p>
+        <p className="introSection__paragraph">
+            <span className ="introSection__paragraph--span">Conscient</span>
+            <span className ="introSection__paragraph--span">des</span>
+            <span className ="introSection__paragraph--span">réalités</span>
+            <span className ="introSection__paragraph--span">opérationnelles,</span>
+            <span className ="introSection__paragraph--span">je</span>
+            <span className ="introSection__paragraph--span">m'efforce</span>
+            <span className ="introSection__paragraph--span">de</span>
+            <span className ="introSection__paragraph--span">comprendre</span>
+            <span className ="introSection__paragraph--span">les</span>
+            <span className ="introSection__paragraph--span">besoins</span>
+            <span className ="introSection__paragraph--span">du</span>
+            <span className ="introSection__paragraph--span">client</span>
+            <span className ="introSection__paragraph--span">afin</span>
+            <span className ="introSection__paragraph--span">de</span>
+            <span className ="introSection__paragraph--span">concevoir</span>
+            <span className ="introSection__paragraph--span">des</span>
+            <span className ="introSection__paragraph--span">solutions</span>
+            <span className ="introSection__paragraph--span">numériques</span>
+            <span className ="introSection__paragraph--span">innovantes.</span>
+        </p>
+        <p className="introSection__paragraph">
+            <span className ="introSection__paragraph--span">Spécialisé</span>
+            <span className ="introSection__paragraph--span">dans</span>
+            <span className ="introSection__paragraph--span">le</span>
+            <span className ="introSection__paragraph--span">développement</span>
+            <span className ="introSection__paragraph--span">React,</span>
+            <span className ="introSection__paragraph--span">je</span>
+            <span className ="introSection__paragraph--span">me</span>
+            <span className ="introSection__paragraph--span">consacre</span>
+            <span className ="introSection__paragraph--span">à</span>
+            <span className ="introSection__paragraph--span">la</span>
+            <span className ="introSection__paragraph--span">conception</span>
+            <span className ="introSection__paragraph--span">d'applications</span>
+            <span className ="introSection__paragraph--span">web</span>
+            <span className ="introSection__paragraph--span">à</span>
+            <span className ="introSection__paragraph--span">la</span>
+            <span className ="introSection__paragraph--span">fois</span>
+            <span className ="introSection__paragraph--span">responsives</span>
+            <span className ="introSection__paragraph--span">et</span>
+            <span className ="introSection__paragraph--span">accessibles.</span>
+        </p>
+        <p className="introSection__paragraph">
+            <span className ="introSection__paragraph--span">Mon</span>
+            <span className ="introSection__paragraph--span">engagement</span>
+            <span className ="introSection__paragraph--span">consiste</span>
+            <span className ="introSection__paragraph--span">à</span>
+            <span className ="introSection__paragraph--span">créer</span>
+            <span className ="introSection__paragraph--span">des</span>
+            <span className ="introSection__paragraph--span">expériences</span>
+            <span className ="introSection__paragraph--span">utilisateur</span>
+            <span className ="introSection__paragraph--span">fluides,</span>
+            <span className ="introSection__paragraph--span">alignées</span>
+            <span className ="introSection__paragraph--span">sur</span>
+            <span className ="introSection__paragraph--span">les</span>
+            <span className ="introSection__paragraph--span">dernières</span>
+            <span className ="introSection__paragraph--span">normes</span>
+            <span className ="introSection__paragraph--span">de</span>
+            <span className ="introSection__paragraph--span">développement</span>
+            <span className ="introSection__paragraph--span">web.</span>
+        </p>  
+    </section>
+  )
+}
+
+export default Intro
